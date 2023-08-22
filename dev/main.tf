b56e7be362d5886b669dda49c96051f048970163
IshaSingh0228 <78353412+IshaSingh0228@users.noreply.github.com>
2023-08-21 23:01:55 -0700

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -6,24 +6,24 @@ provider "aws" {
 }
 
 module "my_vpc" {
-    source ="G:/workspace/terraform2/modules/vpc"
+    source ="../modules/vpc"
     vpc_cidr="192.168.0.0/16"
 }
 
 module "my_subnet"{
-    source="G:/workspace/terraform2/modules/subnets"
+    source="../modules/subnets"
     vpc_id=module.my_vpc.vpc_id
     subnet_cidr="192.168.1.0/24"
     availability_zone="ap-south-1a"
 }
 
 module "my_sg"{
-    source="G:/workspace/terraform2/modules/security_group"
+    source="../modules/security_group"
     vpc_id=module.my_vpc.vpc_id
 }
 
 module "my_ec2"{
-    source="G:/workspace/terraform2/modules/ec2"
+    source="../modules/ec2"
     ami="ami-0d758c1134823146a"
     instance_type="t2.micro"
     subnet_id=module.my_subnet.subnet_id
